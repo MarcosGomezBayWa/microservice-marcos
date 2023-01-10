69ecfbcff361563fc2f4474d96af57ce99581b17
Pascal Paulis <ppaulis@gmail.com>
2023-01-10 10:44:15 +0100

    initial commit
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

---
+++ tests/Acceptance/FeatureContext.php
@@ -0,0 +1,121 @@
+<?php
+
+use BayWaReLusy\BehatContext\AuthContext\AuthContextAwareInterface;
+use BayWaReLusy\BehatContext\AuthContext\AuthContextAwareTrait;
+use BayWaReLusy\BehatContext\ConsoleContext\ConsoleContextAwareInterface;
+use BayWaReLusy\BehatContext\ConsoleContext\ConsoleContextAwareTrait;
+use BayWaReLusy\BehatContext\HalContext\HalContextAwareInterface;
+use BayWaReLusy\BehatContext\HalContext\HalContextAwareTrait;
+use BayWaReLusy\BehatContext\SqsContext\SqsContextAwareInterface;
+use BayWaReLusy\BehatContext\SqsContext\SqsContextAwareTrait;
+use Behat\Behat\Context\Context;
+use Laminas\Mvc\Application as ZfApplication;
+use Laminas\ServiceManager\ServiceManager;
+use Laminas\Stdlib\ArrayUtils;
+use Behat\Behat\Context\Environment\InitializedContextEnvironment;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use BayWaReLusy\QueueTools\QueueTools;
+use BayWaReLusy\QueueTools\QueueService;
+use BayWaReLusy\BehatContext\Auth0Context\MachineToMachineCredentials;
+use BayWaReLusy\BehatContext\Auth0Context\UserCredentials;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements
+    Context,
+    HalContextAwareInterface,
+    ConsoleContextAwareInterface,
+    AuthContextAwareInterface,
+    SqsContextAwareInterface
+{
+    use HalContextAwareTrait;
+    use ConsoleContextAwareTrait;
+    use AuthContextAwareTrait;
+    use SqsContextAwareTrait;
+
+    /** @var ZfApplication */
+    protected ZfApplication $apiToolsApplication;
+
+    protected array $placeholders = [];
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+        require_once __DIR__ . '/../../vendor/autoload.php';
+        date_default_timezone_set('UTC');
+        ini_set('memory_limit', '-1');
+
+        $appConfig = include __DIR__ . '/../../config/application.config.php';
+
+        // Load development config to avoid enabling the config & module cache
+        if (file_exists(__DIR__ . '/../../config/development.config.php')) {
+            $appConfig = ArrayUtils::merge($appConfig, include __DIR__ . '/../../config/development.config.php');
+        }
+
+        $this->apiToolsApplication = ZfApplication::init($appConfig);
+    }
+
+    public function getServiceManager(): ServiceManager
+    {
+        return $this->apiToolsApplication->getServiceManager();
+    }
+
+    /**
+     * @param BeforeScenarioScope $scope
+     * @throws Exception
+     * @BeforeScenario
+     */
+    public function gatherContexts(BeforeScenarioScope $scope): void
+    {
+        if (!is_string(getenv('APACHE_HOSTNAME'))) {
+            throw new Exception('Env var APACHE_HOSTNAME must be set.');
+        }
+
+        /** @var InitializedContextEnvironment $environment */
+        $environment = $scope->getEnvironment();
+
+        $this->gatherConsoleContext($scope);
+
+        $this->gatherHalContext($scope);
+        $this->getHalContext()
+            ->setJsonFilesPath((string)realpath(__DIR__ . '/../../features/_files'))
+            ->setBaseUrl(getenv('APACHE_HOSTNAME'));
+
+        $this->gatherAuthContext($scope);
+        $this->getAuthContext()
+            ->setHalContext($this->getHalContext())
+            ->setServerAddress($this->getServiceManager()->get('config')['auth']['serverAddress'])
+            ->setTokenEndpoint($this->getServiceManager()->get('config')['auth']['tokenEndpoint'])
+//            ->addMachineToMachineCredentials(new MachineToMachineCredentials(
+//                '<client name>',
+//                '<client ID>',
+//                '<client secret>'
+//            ))
+//            ->addUserCredentials(new UserCredentials(
+//                '<username>',
+//                '<password>',
+//                '<client ID>'
+//            ))
+            ;
+
+        $this->gatherSqsContext($scope);
+        $this->getSqsContext()
+            ->setAwsRegion($this->getServiceManager()->get('config')['aws']['region'])
+            ->setAwsKey($this->getServiceManager()->get('config')['aws']['credentials']['key'])
+            ->setAwsSecret($this->getServiceManager()->get('config')['aws']['credentials']['secret'])
+            ->setQueueService($this->getServiceManager()->get(QueueTools::class)->get(QueueService::class))
+//            ->addQueue(new QueueUrl(
+//                '<queue name>',
+//                $this->getServiceManager()->get('config')['queue']['<queue name>']['queueUrl']
+//            ))
+            ;
+    }
+}
